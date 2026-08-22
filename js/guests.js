ad7c7fb7c5c423f49b5eb62d867db0bf84d25774
JaimeGalvezGT <50521876+JaimeGalvezGT@users.noreply.github.com>
2026-08-22 15:10:53 -0600

    Update guests.js

--- js/guests.js
+++ js/guests.js
@@ -54,6 +54,205 @@ const guests = {
 
         seats:2
 
-    }
+    },
 
+        "008":{
+
+        name:"Fam. Ayala Conteras",
+
+        seats:3
+
+    },
+
+        "009":{
+
+        name:"Fam. Ayala Mendoza",
+
+        seats:3
+
+    },
+
+        "010":{
+
+        name:"Fam. Galvez Teo",
+
+        seats:3
+
+    },
+
+        "011":{
+
+        name:"Fam. Galvez Chacon",
+
+        seats:4
+
+    },
+
+        "012":{
+
+        name:"Fam. Ponce Zetino",
+
+        seats:4
+
+    },
+
+        "013":{
+
+        name:"Sr. Gustavo Ayala",
+
+        seats:1
+
+    },
+
+        "014":{
+
+        name:"Sra. Yesenia Gálvez",
+
+        seats:3
+
+    },
+
+        "015":{
+
+        name:"Sr. José Merida",
+
+        seats:1
+
+    },
+
+        "016":{
+
+        name:"Sr. Fredy Fernández",
+
+        seats:1
+
+    },
+
+        "017":{
+
+        name:"Sr. Brandon Fernández",
+
+        seats:1
+
+    },
+
+        "018":{
+
+        name:"Sr. Selvin Cabrera",
+
+        seats:2
+
+    },
+
+        "019":{
+
+        name:"Sr. Luis Cerna",
+
+        seats:1
+
+    },
+
+        "020":{
+
+        name:"Sra. Esperanza Reynosa",
+
+        seats:1
+
+    },
+
+        "021":{
+
+        name:"Sra. Diana Reynosa",
+
+        seats:1
+
+    },
+
+        "022":{
+
+        name:"Fam. Rodriguez Diaz",
+
+        seats:3
+
+    },
+
+        "023":{
+
+        name:"Fam. Gómez Rodríguez",
+
+        seats:6
+
+    },
+
+        "024":{
+
+        name:"Fam. Alvizurez Roche",
+
+        seats:2
+
+    },
+
+        "025":{
+
+        name:"Esposos Reynosa Monterroso",
+
+        seats:2
+
+    },
+
+        "026":{
+
+        name:"Srta. Daniela Melchor",
+
+        seats:1
+
+    },
+
+        "027":{
+
+        name:"Srta. Rocio Hernandez",
+
+        seats:2
+
+    },
+
+        "028":{
+
+        name:"Srta. Daniela Belteton",
+
+        seats:1
+
+    },
+
+        "029":{
+
+        name:"Fam. Melchor Veliz",
+
+        seats:3
+
+    },
+
+        "030":{
+
+        name:"Pastor René Roche",
+
+        seats:2
+
+    },
+
+        "031":{
+
+        name:"Sra. Lilian Perez Rodríguez",
+
+        seats:3
+
+    },
+
+        "032":{
+
+        name:"Sr. Elmer Lopez",
+
+        seats:3
+
+    }
 }
